5e82c12ca959b460abec6849b5017b422d197f77
Garvit <garvit510bhatia@gmail.com>
2024-09-19 18:28:41 +0530

    init

--- server.py
+++ server.py
@@ -1,18 +1,10 @@
-"""
-This is the starting file of the server.
-It serves api request with appropriate response
-"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
-def emo_detector():
-    """
-    It takes input text from user and send to appropriate function, 
-    then return response based on user text emotions. 
-    """
+def detector():
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
     anger = response['anger']
@@ -32,9 +24,6 @@ def emo_detector():
 
 @app.route("/")
 def render_index_page():
-    """
-    It starts server on defined server and port for development. 
-    """
     return render_template('index.html')
 
 if __name__ == "__main__":
